51c4676621844cbcfe4bdaca8be951ddd7f171de
wjagottm <wjagottm@avans.nl>
2019-06-12 13:10:36 +0200

    Fixed dependency issue

--- app/src/main/java/com/blacetec/saws_android/MainActivity.java
+++ app/src/main/java/com/blacetec/saws_android/MainActivity.java
@@ -5,23 +5,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.blacetec.saws_android.liveVideoBroadcaster.LiveVideoBroadcasterActivity;
+import com.blacetec.saws_android.liveVideoBroadcaster.R;
 import com.blacetec.saws_android.liveVideoPlayer.LiveVideoPlayerActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * PLEASE WRITE RTMP BASE URL of the your RTMP SERVER.
-     */
     public static final String RTMP_BASE_URL = "rtmp://141.138.142.48/live/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.blacetec.saws_android.liveVideoBroadcaster.R.layout.activity_main);
+        setContentView(R.layout.activity_main);
     }
 
     public void openVideoBroadcaster(View view) {
-        Intent i = new Intent(this, com.blacetec.saws_android.liveVideoBroadcaster.LiveVideoBroadcasterActivity.class);
+        Intent i = new Intent(this, LiveVideoBroadcasterActivity.class);
         startActivity(i);
     }
 
