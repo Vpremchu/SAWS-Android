c959064a622b4d2ea6179b4516e5229ff84a6cec
wjagottm <wjagottm@avans.nl>
2019-06-12 14:48:00 +0200

    Merged feature/login_activity to feature_Streaming, ready for push to dev

---
+++ app/src/main/java/logic/Utils.java
@@ -0,0 +1,38 @@
+/*
+    Utils.java - Utility functions
+ */
+package logic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Utils {
+    static String getStringFromInputStream(InputStream is) {
+
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+
+            br = new BufferedReader(new InputStreamReader(is));
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
